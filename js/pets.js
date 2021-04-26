20649f6876914acb597946a2a3d5a4c42442b9f9
kam1014 <49304905+kam1014@users.noreply.github.com>
2021-04-26 11:51:52 -0400

    This commit will: add all files

---
+++ js/pets.js
@@ -0,0 +1,10 @@
+/*
+  Create an array of 'pet' objects.
+  Each object should have the following properties: 
+  name, type, breed, age, and photo
+*/
+const pet = [
+  {name: 'name', type: 'type' , breed: 'breed' , age: 'age' , photo: 'photo'},
+  {name: 'name' , type: 'type' , breed: 'breed' , age: 'age' , photo: 'photo'},
+  {name: 'name' , type: 'type'  , breed: 'breed' , age: 'age' , photo: 'photo'}
+]
