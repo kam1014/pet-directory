3ee3e83779703667d643140b98f94bd84114549f
kam1014 <49304905+kam1014@users.noreply.github.com>
2021-04-26 12:16:12 -0400

    This commit will: add object updates

--- js/pets.js
+++ js/pets.js
@@ -4,7 +4,10 @@
   name, type, breed, age, and photo
 */
 const pet = [
-  {name: 'name', type: 'type' , breed: 'breed' , age: 'age' , photo: 'photo'},
-  {name: 'name' , type: 'type' , breed: 'breed' , age: 'age' , photo: 'photo'},
-  {name: 'name' , type: 'type'  , breed: 'breed' , age: 'age' , photo: 'photo'}
+  {name: 'Sammy', type: 'dog' , breed: 'Australian Shepherd' , age: 2 , photo: 'img/aussie.jpg'},
+  {name: 'Max' , type: 'dog' , breed: 'Smooth Dachshund' , age: 1 , photo: 'img/dachshund.jpg'},
+  {name: 'Stella' , type: 'dog'  , breed: 'Golden Retriever' , age: 4 , photo: 'img/golden.jpg'},
+  {name: 'lucky' , type: 'cat'  , breed: 'Himalayan Persian' , age: 3 , photo: 'img/persian.jpg'},
+  {name: 'Betsy' , type: 'dog'  , breed: 'Fawn Pug' , age: 1 , photo: 'img/pug.jpg'},
+  {name: 'Kit' , type: 'cat'  , breed: 'Classic Tabby' , age: 3 , photo: 'img/tabby.jpg'}
 ]
