3cd5dbd1e2216d702eb3ffbf8f8f6ed84f38a93a
kam1014 <49304905+kam1014@users.noreply.github.com>
2021-04-26 14:04:45 -0400

    This commit will: add final update

--- js/pets.js
+++ js/pets.js
@@ -3,11 +3,44 @@
   Each object should have the following properties: 
   name, type, breed, age, and photo
 */
-const pet = [
-  {name: 'Sammy', type: 'dog' , breed: 'Australian Shepherd' , age: 2 , photo: 'img/aussie.jpg'},
-  {name: 'Max' , type: 'dog' , breed: 'Smooth Dachshund' , age: 1 , photo: 'img/dachshund.jpg'},
-  {name: 'Stella' , type: 'dog'  , breed: 'Golden Retriever' , age: 4 , photo: 'img/golden.jpg'},
-  {name: 'lucky' , type: 'cat'  , breed: 'Himalayan Persian' , age: 3 , photo: 'img/persian.jpg'},
-  {name: 'Betsy' , type: 'dog'  , breed: 'Fawn Pug' , age: 1 , photo: 'img/pug.jpg'},
-  {name: 'Kit' , type: 'cat'  , breed: 'Classic Tabby' , age: 3 , photo: 'img/tabby.jpg'}
+
+// let p_name = '';
+// let p_type = '';
+// let p_breed = '';
+// let p_age = 0;
+// let p_image = ``;
+
+let pets = [
+  {name: 'Sammy', type: 'Dog' , breed: 'Australian Shepherd' , age: 2 , photo: 'img/aussie.jpg'},
+  {name: 'Max' , type: 'Dog' , breed: 'Smooth Dachshund' , age: 1 , photo: 'img/dachshund.jpg'},
+  {name: 'Stella' , type: 'Dog'  , breed: 'Golden Retriever' , age: 4 , photo: 'img/golden.jpg'},
+  {name: 'lucky' , type: 'Cat'  , breed: 'Himalayan Persian' , age: 3 , photo: 'img/persian.jpg'},
+  {name: 'Betsy' , type: 'Dog'  , breed: 'Fawn Pug' , age: 1 , photo: 'img/pug.jpg'},
+  {name: 'Kit' , type: 'Cat'  , breed: 'Classic Tabby' , age: 3 , photo: 'img/tabby.jpg'}
 ]
+
+
+function createListItems(arr) {
+ 
+let items = ''; 
+
+for ( let i = 0; i < arr.length; i++ ) {
+
+  items += `
+  <ul>
+  <h2>${pets[i].name}</h2>
+  <h3>${pets[i].type} | ${pets[i].breed}</h3>
+  <p> Age: ${pets[i].age}</p>
+  <img src ="${pets[i].photo}">
+  </ul>`;
+}
+return items;
+}
+
+
+let html = `${createListItems(pets)}`;
+
+// console.log(html);
+
+
+document.querySelector('main').innerHTML = html ; 
